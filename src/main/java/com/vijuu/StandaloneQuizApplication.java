075a26e3bf57cc9c9d1bf693bba30fe7a48852e8
vijay2633 <vijayviju456@gmail.com>
2025-09-07 12:11:51 +0530

    initial commiit

---
+++ src/main/java/com/vijuu/StandaloneQuizApplication.java
@@ -0,0 +1,13 @@
+package com.vijuu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandaloneQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandaloneQuizApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vijuu/controller/QuizController.java
@@ -0,0 +1,17 @@
+package com.vijuu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuizController {
+	
+	
+	@GetMapping("/hello")
+	public String getMsg() {
+		return "hey hi";
+	}
+	
+//	gssdgasdgasdgasd
+
+}
