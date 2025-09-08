c06d11f648e80cd70a1d593477ef7937ccb8a698
vijay2633 <vijayviju456@gmail.com>
2025-09-08 13:35:11 +0530

    question controller API's completed

---
+++ src/main/java/com/vijuu/service/QuizServiceImpl.java
@@ -0,0 +1,32 @@
+package com.vijuu.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vijuu.entity.Question;
+import com.vijuu.entity.Quiz;
+import com.vijuu.repository.QuestionRepository;
+import com.vijuu.repository.QuizRepository;
+
+@Service
+public class QuizServiceImpl implements QuizService {
+
+	@Autowired
+	private QuestionRepository questionRepo;
+	
+	@Autowired
+	private QuizRepository quizRepo;
+
+	@Override
+	public String createQuiz(String category, Integer numQ, String title) {
+		List<Question> questions = questionRepo.findRandomQuestionsByCategory(category, numQ);
+		Quiz quiz = new Quiz();
+		quiz.setQuestion(questions);
+		quiz.setTitle(title);
+		quizRepo.save(quiz);
+		return "quiz created successfully";
+	}
+
+}
